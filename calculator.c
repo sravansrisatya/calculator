0ac9c28db01f9a9d8d24eea90f311d59c3de7bbf
Sravan Sri Satya Chodapaneedi <24je0607@iitism.ac.in>
2024-10-24 10:07:02 +0530

    Square and Cube implemented

--- calculator.c
+++ calculator.c
@@ -64,12 +64,12 @@ int main() {
             case '^':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("The Square of %d is %d\n",a,a*a);
                 break;
             case '#':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("The Cube of %d is %d\n",a,a*a*a);
                 break;
             case 'i':
                 printf("Enter an integer: ");
